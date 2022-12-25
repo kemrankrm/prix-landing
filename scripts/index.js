70c37e5dd9e40d29c952704863d2bf6d5609a7a9
Kemran Karimov <kemran.karimov@gmail.com>
2022-12-25 19:02:20 +0600

    script optimization

--- scripts/index.js
+++ scripts/index.js
@@ -5,56 +5,48 @@ const advantagesTitleBorder = document.querySelector('.advantages__title-border'
 
 const scrollDisplay = document.querySelector('.scroll');
 
+//FUNCTIONS
+const varyBorderWidth = (titleBorder, minScroll, maxScroll) => {
+    let contentBorderWidth = (scrollPosition - minScroll)*(100 / (maxScroll - minScroll));
+    if (scrollPosition < minScroll) {
+        titleBorder.style.width = "0%"
+    } else if (scrollPosition > maxScroll) {
+        titleBorder.style.width = '100%';
+    } else {
+        titleBorder.style.width = `${contentBorderWidth < 100 ? contentBorderWidth : 100}%`
+    }
+}
 
-console.log(featuresHeader)
+const varyElementOpacity = (element, minScroll, maxScroll) => {
+    if (scrollPosition < minScroll) {
+        element.setAttribute('style', 'opacity: 0')
+    } else if (scrollPosition > maxScroll) {
+        element.setAttribute('style', 'opacity: 1;')
+    } else {
+        element.setAttribute('style', `opacity: ${(scrollPosition - minScroll) * (1 / (maxScroll - minScroll))}`)
+    }
+}
+
 let scrollPosition;
+
 //FEATURES HEADER VARIABLE OPACITY SETUP
 window.addEventListener('scroll', () => {
-    console.log('SCROLL: ', window.pageYOffset);
     scrollPosition = window.pageYOffset;
-    if (scrollPosition < 400) {
-        featuresHeader.setAttribute('style', 'opacity: 0')
-    } else if (scrollPosition > 580) {
-        featuresHeader.setAttribute('style', 'opacity: 1;')
-    } else {
-        featuresHeader.setAttribute('style', `opacity: ${(scrollPosition - 400) * 0.006}`)
-    }
-
     scrollDisplay.textContent = scrollPosition;
-    let contentBorderWidth = (scrollPosition - 1171)*0.371;
-    if (scrollPosition < 1171) {
-        featuresTitleBorder.style.width = "0%"
-    } else if (scrollPosition > 1440) {
-        featuresTitleBorder.style.width = '100%';
-    } else {
-        featuresTitleBorder.style.width = `${contentBorderWidth < 100 ? contentBorderWidth : 100}%`
-    }
 
-    let advantagesBorderWidth = (scrollPosition - 1820)*0.3226;
-    if (scrollPosition < 1820) {
-        advantagesTitleBorder.style.width = "0%"
-    } else if (scrollPosition > 2130) {
-        advantagesBorderWidth.style.width = '100%';
-    } else {
-        advantagesTitleBorder.style.width = `${advantagesBorderWidth < 100 ? advantagesBorderWidth : 100}%`
-    }
+    //VARIABLE CARDS TITLE OPACITY
+    varyElementOpacity(featuresHeader, 400, 580);
 
-    // featuresCards.forEach(item => {
-    //     if (scrollPosition < 670) {
-    //         item.setAttribute('style', 'opacity: 0')
-    //     // } else if (scrollPosition > 1000) {
-    //     //     item.setAttribute('style', 'opacity: 1')
-    //     } else {
-    //         item.setAttribute('style', `opacity: ${(scrollPosition - 670) * 0.003}`)
-    //     }
-    // })
-    //
-    // // if (scrollPosition < 670) {
-    // //     featuresCardsBlock.setAttribute('style', 'opacity: 0');
-    // // } else if (scrollPosition > 1000) {
-    // //     featuresCardsBlock.setAttribute('style', 'opacity: 1')
-    // // } else {
-    // //     featuresCardsBlock.setAttribute('style', `opacity: ${(scrollPosition - 670) * 0.003}`)
-    // // }
+    //VARIABLE OPACITY FOR EACH CAR
+    featuresCards.forEach(item => {
+        varyElementOpacity(item, 600, 800)
+    })
+
+    //VARIABLE BORDER WIDTH FOR CONTENT SECTION
+    varyBorderWidth(featuresTitleBorder, 1171, 1600);
+
+    //VARIABLE BORDER WIDTH FOR ADVANTAGE SECTION
+    varyBorderWidth(advantagesTitleBorder, 1820, 2130);
+
 })
 
