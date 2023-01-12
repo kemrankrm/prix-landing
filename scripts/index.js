0e3f5773b18864c6c4153e4d0dd483655386ff29
Daniyar Otynshin <daniyar.otynshin@gmail.com>
2023-01-12 09:07:07 +0600

    change intro background and combine styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,3 @@
-const popupContent = [
-    {
-        name: 'marketplace',
-        title: 'Удобный маркетплейс',
-        text: 'PRIX предоставляет выбор авто из самой актуальной базы зарубежных авторынков. Подробное описание автомобиля, фото и цены от наших партнеров - все это будет компактно упаковано для приятного чтения. Команда Prix делает все для упрощения и прозрачности покупки авто. Осталось приступить к оформлению заказа. Удачи!',
-        image: './images/porsche-minimalist-4k-s4-1366x768.jpg',
-        addition: 'blablabla',
-    },
-    {
-        name: 'pricing',
-        title: 'Лучшие цены',
-        text: 'Основная цель Prix - предоставить лучшие условия для клиентов. Мы стимулируем наших партнеров конкурировать для предоставления лучших цен и снижение сроков поставки. Помимо цен ты также сможешь увидеть преимущества каждой компании, отзывы и оценки наших клиентов, а также количество успешно доставленных автомобилей. Мы уверены, что ты найдешь ту самую!',
-        image: './images/bestPricePopupImage.jpg',
-        addition: 'blablabla',
-    },
-    {
-        name: 'gps',
-        title: 'GPS ТРЕКИНГ',
-        text: 'Безопасность сделки - один из главных приоритетов Prix. Команда разработчиков позаботилась о твоих нервах и внедрила точную систему определения местоположения авто в любой точке мира(GPS). Удобный интерфейс поможет тебе всегда знать, где в данный момент находится твой автомобиль. Также перед отправкой тебе будет выслан подробный отчет об осмотре авто специалистом, дополненный фотографиями. После подписания договора в личном кабинете появится возможность отслеживания авто. Не переживай!',
-        image: './images/gpsPicForPopup.jpeg',
-        addition: 'blablabla',
-    },
-]
 const cardsContent = [
     {
         id: 'marketplace',
@@ -52,72 +29,14 @@ const cardsContent = [
 const cardsContainer = document.querySelector('.features__cards');
 const cardTemplate = document.querySelector('.card-template').content;
 
-const featuresHeader = document.querySelector('.features__header');
 const featuresCards = document.querySelectorAll('.features__card');
 const featuresTitleBorder = document.querySelector('.content__title-border');
 const advantagesTitleBorder = document.querySelector('.advantages__title-border');
 
-const blogTitle = document.querySelector('.blog__title');
-const blogSubtitle = document.querySelector('.blog__subtitle');
-const blogContainer = document.querySelector('.blog__container');
-const blogImageDescription = document.querySelectorAll('.blog__description');
-
-const popup = document.querySelector('.popup');
-const popupCloseButton = document.querySelector('.popup__close-button');
-const popupOverlay = document.querySelector('.popup__overlay');
-const popupImage = popup.querySelector('.popup__image')
-const popupTitle = popup.querySelector('.popup__title');
-const popupText = popup.querySelector('.popup__text');
-
-
 //CARD ELEMENT CLONING
 cloneCardElement(cardsContent);
 
-const closeElements = [popupCloseButton, popupOverlay];
-const cards = document.querySelectorAll('.features__card');
-
-//CARD ELEMENT CLICK LISTENER
-// cards.forEach(item => {
-//     item.addEventListener('click', () => handlePopupOpen(item));
-// })
-
-closeElements.forEach(item => {
-    item.addEventListener('click', handlePopupClose)
-})
-
 const scrollDisplay = document.querySelector('.scroll');
-
-// function handlePopupOpen(item) {
-//     const content = popupContent.find(block => block.name === item.id)
-//
-//     popupImage.setAttribute('src', content.image)
-//     popupTitle.textContent = content.title;
-//     popupText.textContent = content.text;
-//
-//     popup.classList.add('popup_visible');
-//     document.addEventListener('keydown', handleEscClose)
-//
-// }
-
-function handlePopupClose() {
-    popup.classList.remove('popup_visible');
-    document.removeEventListener('keydown', handleEscClose);
-    clearPopupData();
-}
-
-function clearPopupData() {
-    popupImage.setAttribute('src', null)
-    popupTitle.textContent = null;
-    popupText.textContent = null;
-}
-
-function handleEscClose(e) {
-    let button = e.key;
-
-    if (button === 'Escape') {
-        handlePopupClose()
-    }
-}
 
 function cloneCardElement (data) {
     data.forEach(item => {
@@ -138,7 +57,8 @@ function cloneCardElement (data) {
 
 //FUNCTIONS
 const varyBorderWidth = (titleBorder, minScroll, maxScroll) => {
-    let contentBorderWidth = (scrollPosition - minScroll)*(100 / (maxScroll - minScroll));
+    let contentBorderWidth = (scrollPosition - minScroll) * (100 / (maxScroll - minScroll));
+
     if (scrollPosition < minScroll) {
         titleBorder.style.width = "0%"
     } else if (scrollPosition > maxScroll) {
@@ -160,21 +80,10 @@ const varyElementOpacity = (element, minScroll, maxScroll) => {
 
 let scrollPosition;
 
-console.log(window.visualViewport)
 //FEATURES HEADER VARIABLE OPACITY SETUP
 window.addEventListener('scroll', () => {
     scrollPosition = window.pageYOffset;
     scrollDisplay.textContent = window.screen.width;
-
-    //VARIABLE CARDS TITLE OPACITY
-    // varyElementOpacity(featuresHeader, 400, 580);
-
-    // varyElementOpacity(blogTitle, 2479, 2850);
-    // varyElementOpacity(blogSubtitle, 2546, 2900);
-    // varyElementOpacity(blogContainer, 2700, 2900);
-    // blogImageDescription.forEach(item => {
-    //     varyElementOpacity(item, 2900, 3050);
-    // })
 
     //VARIABLE OPACITY FOR EACH CAR
     featuresCards.forEach(item => {
